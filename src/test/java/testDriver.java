3785cc4e9f8a21260afa96f18bb35f7fa232bf9b
vikramvi <vikram@6wunderkinder.com>
2015-05-06 18:12:58 +0200

    screen shot capturing logic added

--- src/test/java/testDriver.java
+++ src/test/java/testDriver.java
@@ -1,4 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,6 +17,16 @@ public class testDriver {
 		 WebDriver driver = new ChromeDriver();
 		 driver.get(url);
 		 String title = driver.getTitle();
+		 
+		 File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		
+		try {
+			FileUtils.copyFile(scrFile, new File("screenshot.png"));
+		} catch (IOException e) {
+		
+			e.printStackTrace();
+		}
+		 
 		 System.out.println("The title of " + url + " is: " + title);
 		 driver.quit();
 	}
